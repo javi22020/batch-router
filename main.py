ce0d875a514b6c9227c1c235459bee50948c45c0
javi22020 <90896750+javi22020@users.noreply.github.com>
2025-11-17 14:23:29 +0100

    Refactor input batch creation in main.py for improved readability
    - Simplified the structure of input batch requests by defining them in a separate list before creating the InputBatch.
    - Updated the sleep duration in the polling loop from 3 to 5 seconds for better status checking.
    - Added comments to clarify the potential for using the same input batch with different providers.

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ from batch_router.core.base import TextContent, InferenceParams
 
 provider = vLLMProvider(model_path="./gemma_3_270m")
 
-input_batch = InputBatch(
-    requests=[
-        InputRequest(
-            custom_id=f"request_{i+1}",
-            messages=[
-                InputMessage(
-                    role=InputMessageRole.USER,
-                    contents=[
-                        TextContent(text="Hello, how are you?")
-                    ]
-                )
-            ],
-            params=InferenceParams(
-                max_output_tokens=100
+requests = [
+    InputRequest(
+        custom_id=f"request_{i+1}",
+        messages=[
+            InputMessage(
+                role=InputMessageRole.USER,
+                contents=[
+                    TextContent(text="Hello, how are you?")
+                ]
             )
+        ],
+        params=InferenceParams(
+            max_output_tokens=100
         )
-        for i in range(10)
-    ]
+    )
+    for i in range(10)
+]
+input_batch = InputBatch(
+    requests=requests
 )
 
 vllm_batch = input_batch.with_config(
@@ -33,10 +34,18 @@ vllm_batch = input_batch.with_config(
     )
 )
 
+# You can also use the same input batch for multiple providers by configuring it with different models and providers.
+# openai_batch = input_batch.with_config(
+#     config=BatchConfig(
+#         provider_id=ProviderId.OPENAI,
+#         model_id="gpt-5-mini"
+#     )
+# )
+
 vllm_batch_id = provider.send_batch(vllm_batch)
 
 while provider.poll_status(vllm_batch_id) != BatchStatus.COMPLETED:
-    time.sleep(3)
+    time.sleep(5)
     print(f"Batch {vllm_batch_id} is {provider.poll_status(vllm_batch_id)}")
 
 vllm_batch = provider.get_results(vllm_batch_id)
